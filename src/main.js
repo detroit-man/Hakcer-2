c865d2404f5d00cdcfa3e668822ca7ca82bc4bef
Detroit Man <101349397+detroit-man@users.noreply.github.com>
2023-04-18 10:43:07 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ if(!localStorage.getItem('cl')) {
   localStorage.setItem('cl', '0')
 }
 
+let l = {
+  unset: '0',
+  none: '',
+  char: 'abcdefghijklmnopqrstuvwxyz125467890ABCDEFGHIJKLMNOPQRSTUVWXYZ.();',
+  num: '124567890'
+}
+
+function switchTo(char) {
+    localStorage.setItem('cl', l[char])
+}
+
 document.body.onkeydown = function()
 {
   c=localStorage.getItem('cl') // automatic unloaded
