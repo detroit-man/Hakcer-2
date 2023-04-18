b57a5bfe7fcf5b9cf072e97aba4d101b74551611
Detroit Man <101349397+detroit-man@users.noreply.github.com>
2023-04-18 10:43:18 -0500

    Delete symbols.js

--- src/symbols.js
+++
@@ -1,7 +0,0 @@
-document.body.onkeydown = function()
-{
-  c='abcdefghijklmnopqrstuvwxyz125467890ABCDEFGHIJKLMNOPQRSTUVWXYZ.();'
-  t=[]
-  c.split("").forEach(function(l){t.push(l)})
-  document.body.innerHTML += t[Math.floor(Math.random()*t.length)] + '\n'
-}
